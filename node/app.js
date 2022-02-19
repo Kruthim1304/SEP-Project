57894aa081a0637a1de29298d528af991779b00f
Kruthi M <84682275+Kruthim1304@users.noreply.github.com>
2022-02-19 20:45:27 +0530

    Backend Files

---
+++ node/app.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const fs = require('fs')
+const port = 6969
+
+const server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/html'})
+    fs.readFile('index.html', function(error, data){
+        if (error){
+            res.writeHead(404)
+            res.write('error 404:file not found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port, function(error){
+    if(error)
+    console.log('something went wrong',error)
+    else
+    console.log('Server is listening to port',+ port)
+})
